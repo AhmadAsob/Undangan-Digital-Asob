8359abf4a85bcb97f18136273c62d370c1283da4
Asob <ahmad.sobari@skyworx.co.id>
2026-05-23 18:42:01 +0700

    Fixing ig yola

--- src/components/BrideGroom.jsx
+++ src/components/BrideGroom.jsx
@@ -104,7 +104,7 @@ const BrideGroom = () => {
                   borderColor: 'rgba(255, 255, 255, 0.7)'
                 }}
                 whileTap={{ scale: 0.95 }}
-                href="https://www.instagram.com/yolandaazzaahraa/"
+                href="https://www.instagram.com/yolandaazzahraa/"
                 target="_blank"
                 rel="noreferrer"
                 style={styles.igLink}
@@ -122,7 +122,7 @@ const BrideGroom = () => {
                   <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
                   <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
                 </svg>
-                @yolandaazzaahraa
+                @yolandaazzahraa
               </motion.a>
             </motion.div>
           </div>
